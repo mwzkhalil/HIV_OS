c67cedcdba41b63eb9ad0241914944e34cb49264
mahwiz khalil <77918472+mwzkhalil@users.noreply.github.com>
2024-05-22 01:45:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from api import apikey
 from flask import Flask, request, jsonify
 import pandas as pd
 from csv import writer
+from transformers import AutoTokenizer
+import transformers
+import torch
 
 import os
 import time
-import openai
 import json
 import jsonpickle
 
-apikeys = apikey
 
 app = Flask(__name__)
+
+
+#specify the model you want to use
+model = "meta-llama/Meta-Llama-3-8B"
+
+#load the pretrained tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+#make it ready for text generation
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+def get_response_llama2(prompt):
+
+  sequences = pipeline(prompt,
+      do_sample=True,
+      return_full_text=False,
+      top_k=10,
+      num_return_sequences=1,
+      eos_token_id=tokenizer.eos_token_id,
+      max_length=4096,
+  )
+  return sequences[0]['generated_text']
 
 
 ############## GPT PROMPT ####################
@@ -95,16 +122,26 @@ you have to follow these cases according to the user needs
 
               ```
     """}
-    new_inp = inp
-    new_inp.insert(0,systems)
-    print("inp : \n ",new_inp)
-    openai.api_key = apikeys
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=new_inp
+
+
+    # Ensure the input is a list of messages
+    if isinstance(inp, str):
+        user_message = {"role": "user", "content": inp}
+        new_inp = [systems, user_message]
+    elif isinstance(inp, list):
+        new_inp = [systems] + inp
+    else:
+        raise ValueError("Input should be a string or a list of messages")
+    
+    # print("inp : \n", new_inp)
+    # new_inp = inp
+    # new_inp.insert(0,systems)
+    # print("inp : \n ",new_inp)
+    completion = get_response_llama2(
+    new_inp
     )
     return completion
-
+	
 ############    GET CHATS BY USER ID ##################
 def get_chats(id):
     path = str(os.getcwd())+'\\chats\\'+id+'.json'
@@ -115,9 +152,6 @@ def get_chats(id):
         return  list(chats)
     else:
         return "No Chat found on this User ID."
-
-
-
 
 
 ############### APPEND NEW CHAT TO USER ID JSON FILE #################
